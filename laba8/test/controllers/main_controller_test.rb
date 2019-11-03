aa09a4841365483162e37454c5d14c1fb0f45002
tetovske <tetovske@pop-os.localdomain>
2019-11-03 22:20:02 +0300

    yay!

--- laba8/test/controllers/main_controller_test.rb
+++ laba8/test/controllers/main_controller_test.rb
@@ -4,4 +4,23 @@ class MainControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'go to the main page' do
+    get index_url
+    assert_response :success
+  end
+
+  test 'go to the responce page' do
+    get result_url
+    assert_response :success
+  end
+
+  test 'test values' do
+    get result_url, params: { n: 300 }
+    assert_equal assigns[:res].first, [220, 284]
+  end
+
+  test 'test empty responce' do
+    get result_url, params: { n: 0 }
+    assert assigns[:res].empty?
+  end
 end
